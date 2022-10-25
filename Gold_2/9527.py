70b8b7488624b2ea535de33c4b1e1f72960727f2
yr7256 <yr7256@gmail.com>
2022-10-25 15:11:37 +0900

    9527

---
+++ Gold_2/9527.py
@@ -0,0 +1,18 @@
+import sys
+from math import log2
+
+
+def onecnt(n):
+    if n <= 0:
+        return 0
+    square = 2**int(log2(n))
+    diff = n - square
+    if diff == 0:
+        return int(log2(n))*n//2+1
+    return onecnt(square)+onecnt(diff)+diff
+    
+
+
+input = sys.stdin.readline
+A, B = map(int, input().split())
+print(onecnt(B)-onecnt(A-1))
