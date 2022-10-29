4866c959b526d593667379fcbd32d25e7bdf6feb
yr7256 <yr7256@gmail.com>
2022-10-29 21:53:56 +0900

    1029

---
+++ Gold_4/9252.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+s1 = ' '+input().rstrip()
+s2 = ' '+input().rstrip()
+dp = [['']*(len(s2)) for _ in range(len(s1))]
+for i in range(1, len(s1)):
+    for j in range(1, len(s2)):
+        if s1[i] == s2[j]:
+            dp[i][j] = dp[i-1][j-1] + s1[i]
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+print(len(dp[-1][-1]))
+if len(dp[-1][-1]):
+    print(dp[-1][-1])
