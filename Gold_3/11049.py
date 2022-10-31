6f4ab35ed844b65cd5d4b8f3c50d376929405672
yr7256 <yr7256@gmail.com>
2022-10-31 17:45:46 +0900

    11049 아직 못풀었음

--- Gold_3/11049.py
+++ Gold_3/11049.py
@@ -1,3 +1,13 @@
 N = int(input())
-for _ in range(N):
-    r, c = map(int, input().split())
+dp = [[0]*N for _ in range(N)]
+matrix = [list(map(int, input().split())) for _ in range(N)]
+# 이 아이디어로 접근해보자.
+for i in range(N):
+    for start in range(N-i):
+        end = start+i
+        maxnum = 2**32
+        for next in range(start, end):
+            dp[start][end] = min(dp[start][end])
+
+
+dp[start][0]*dp[next][1]*dp[end][1]
