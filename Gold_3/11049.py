c2b08dec85c880652a430301c8c12ef6bf725cc3
yr7256 <yr7256@gmail.com>
2022-11-01 15:52:47 +0900

    11049 아직

--- Gold_3/11049.py
+++ Gold_3/11049.py
@@ -1,13 +1,16 @@
 N = int(input())
 dp = [[0]*N for _ in range(N)]
 matrix = [list(map(int, input().split())) for _ in range(N)]
-# 이 아이디어로 접근해보자.
-for i in range(N):
+# 이 아이디어로 접근해보자. 늘어나게
+for i in range(1, N):
     for start in range(N-i):
         end = start+i
-        maxnum = 2**32
-        for next in range(start, end):
-            dp[start][end] = min(dp[start][end])
+        dp[start][end] = 2**32
+        for mid in range(start, end):
+            dp[start][end] = min(dp[start][end], dp[start][mid]+dp[mid+1]
+                                 [end]+matrix[start][0]*matrix[mid][1]*matrix[end][1])
 
-
-dp[start][0]*dp[next][1]*dp[end][1]
+ans = 0
+for i in dp:
+    ans = max(ans, max(i))
+print(ans)
