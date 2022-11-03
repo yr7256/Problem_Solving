ca61a3ca1a5d074492a31bf24708f69df39ad695
yr7256 <yr7256@gmail.com>
2022-11-03 16:02:41 +0900

    1103

---
+++ Gold_5/14719.py
@@ -0,0 +1,10 @@
+H, W = map(int, input().split())
+lst = list(map(int, input().split()))
+ans = 0
+for i in range(1, W-1):
+    column = min(max(lst[:i]), max(lst[i+1:]))
+    if column > lst[i]:
+        ans += column - lst[i]
+    print(lst[:i], lst[i+1:])
+    print(column)
+print(ans)
