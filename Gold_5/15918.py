69c09af8379eceef28bd07530cb6715bcad42a1f
yr7256 <yr7256@gmail.com>
2023-05-10 17:31:24 +0900

    문제풀이

--- Gold_5/15918.py
+++ Gold_5/15918.py
@@ -2,20 +2,20 @@ def dfs(x):
     global ans
     if x == 2*n+1:
         ans += 1
-        print(arr)
+        # print(arr)
         return
     if arr[x]:
         dfs(x+1)
     else:
-        for i in range(n):
-            if not visited[i+1] and x+i+2 < 2*n+1 and not (arr[x] or arr[x+i+2]):
-                visited[i+1] = 1
-                arr[x] = i+1
-                arr[x+i+2] = i+1
+        for i in range(1, n+1):
+            if not visited[i] and x+i+1 < 2*n+1 and not arr[x+i+1]:
+                visited[i] = 1
+                arr[x] = i
+                arr[x+i+1] = i
                 dfs(x+1)
-                visited[i+1] = 0
+                visited[i] = 0
                 arr[x] = 0
-                arr[x+i+2] = i+1
+                arr[x+i+1] = 0
 
 
 n, x, y = map(int, input().split())
