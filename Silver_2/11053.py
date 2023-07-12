db8ae34a57204952168462c7639450dcfcc1b266
김영록 <yr7256@gmail.com>
2023-07-12 23:46:20 +0900

    LIS2

--- Silver_2/11053.py
+++ Silver_2/11053.py
@@ -1,8 +1,8 @@
 N = int(input())
 A = list(map(int, input().split()))
-B = [1]*N
+dp = [1]*N
 for i in range(1, N):
     for j in range(i):
-        if A[i] > A[j] and B[i] < B[j]+1:
-            B[i] = B[j]+1
-print(max(B))
+        if A[i] > A[j] and dp[i] < dp[j]+1:
+            dp[i] = dp[j]+1
+print(max(dp))
