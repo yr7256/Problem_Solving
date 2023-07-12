c505329b32f0ae561873b842d0c24938f2ba840b
김영록 <yr7256@gmail.com>
2023-07-12 23:47:00 +0900

    LIS2

--- Gold_2/12015.py
+++ Gold_2/12015.py
@@ -18,3 +18,20 @@ for num in A:
     else:
         lst[bisect(num)] = num
 print(len(lst)-1)
+
+# N = int(input())
+# A = list(map(int, input().split()))
+# lst = [0]
+# for num in A:
+#     if lst[-1] < num:
+#         lst.append(num)
+#     else:
+#         s, e = 0, len(lst)
+#         while s < e:
+#             m = (s+e)//2
+#             if lst[m] < num:
+#                 s = m+1
+#             else:
+#                 e = m
+#         lst[e] = num
+# print(len(lst)-1)
