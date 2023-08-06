a130383fb8e672c819f1e18de887406685d79abb
김영록 <yr7256@gmail.com>
2023-08-07 04:26:30 +0900

    문제정리

---
+++ Platinum_5/9376.py
@@ -0,0 +1,46 @@
+# 빈 공간은 '.', 지나갈 수 없는 벽은 '*', 문은 '#', 죄수의 위치는 '$'
+from collections import deque
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+T = int(input())
+for _ in range(T):
+    h, w = map(int, input().split())
+    arr = []
+    people = []
+    for i in range(h):
+        arr.append(list(input()))
+        for j in range(w):
+            if arr[i][j] == '$':
+                people.append([i, j])
+    
+
+
+'''
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+N, M = map(int, input().split())
+field = [list(map(int, input().rstrip())) for _ in range(N)]
+visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
+visited[0][0][0] = 1  # x값, y값, 벽뚫 안뚫
+queue = deque([[0, 0, 0]])
+while queue:
+    x, y, destroy = queue.popleft()
+    if x == N-1 and y == M-1:
+        print(visited[x][y][destroy])
+        break
+    for i in range(4):
+        x1 = x+dx[i]
+        y1 = y+dy[i]
+        if 0 <= x1 < N and 0 <= y1 < M:
+            if field[x1][y1] == 0 and visited[x1][y1][destroy] == 0:
+                visited[x1][y1][destroy] = visited[x][y][destroy]+1
+                queue.append([x1, y1, destroy])
+            elif field[x1][y1] == 1 and destroy == 0:
+                visited[x1][y1][1] = visited[x][y][0]+1
+                queue.append([x1, y1, 1])
+else:
+    print(-1)
+'''
