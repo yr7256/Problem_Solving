eb4670f67c85eb8489f2d2091d43350a6bb4cc6e
김영록 <yr7256@gmail.com>
2023-11-24 05:46:25 +0900

    bfs

---
+++ Silver_2/24444.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+arr = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+s = 1
+for _ in range(M):
+    u, v = map(int, input().split())
+    arr[u].append(v)
+    arr[v].append(u)
+queue = deque([R])
+visited[R] = 1
+while queue:
+    curr = queue.popleft()
+    arr[curr].sort()
+    for dest in arr[curr]:
+        if not visited[dest]:
+            s += 1
+            visited[dest] = s
+            queue.append(dest)
+for i in visited[1:]:
+    print(i)
