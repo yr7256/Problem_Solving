a9de32921cb1dd8583955f6089163c7e3af8dc36
김영록 <yr7256@gmail.com>
2023-12-09 05:32:09 +0900

    문제정리

---
+++ Bronze_5/30676.py
@@ -0,0 +1,15 @@
+l = int(input())
+if 380 <= l < 425:
+    print('Violet')
+elif 425 <= l < 450:
+    print('Indigo')
+elif 450 <= l < 495:
+    print('Blue')
+elif 495 <= l < 570:
+    print('Green')
+elif 570 <= l < 590:
+    print('Yellow')
+elif 590 <= l < 620:
+    print('Orange')
+elif 620 <= l <= 780:
+    print('Red')
