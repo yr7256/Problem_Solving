74d846ac992b114af886ec831ba1ee13a575cefb
KIM YOUNGROK <yr7256@gmail.com>
2024-07-18 02:52:23 +0900

    14888

---
+++ Silver_2/14888.py
@@ -0,0 +1,24 @@
+def solution(num, answer, pl, mi, multi, div):
+    global maximum, minimum
+    if num == N:
+        maximum = max(maximum, answer)
+        minimum = min(minimum, answer)
+        return
+    if pl:
+        solution(num+1, answer+A[num], pl-1, mi, multi, div)
+    if mi:
+        solution(num+1, answer-A[num], pl, mi-1, multi, div)
+    if multi:
+        solution(num+1, answer*A[num], pl, mi, multi-1, div)
+    if div:
+        solution(num+1, int(answer/A[num]), pl, mi, multi, div-1)
+
+
+N = int(input())
+A = list(map(int, input().split()))
+oper = list(map(int, input().split()))
+maximum = -1e9
+minimum = 1e9
+solution(1, A[0], oper[0], oper[1], oper[2], oper[3])
+print(maximum)
+print(minimum)
