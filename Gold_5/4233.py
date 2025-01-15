895ee08187e44437834695267e42e090655fbf43
KIM YOUNGROK <yr7256@gmail.com>
2025-01-16 00:45:53 +0900

    boj 4233

--- Gold_5/4233.py
+++ Gold_5/4233.py
@@ -8,11 +8,23 @@ def isprime(x):
         return True
 
 
+def test(x, p):  # x: 밑 , p : 소수판정
+    ans = 1
+    num = x
+    exponent = p
+    while exponent:
+        if exponent % 2:
+            ans = (ans * num) % p
+        num = (num * num) % p
+        exponent //= 2
+    return ans
+
+
 while True:
     p, a = map(int, input().split())
     if p == 0 and a == 0:
         break
-    if not isprime(p):
-        print('Yes')
+    if not isprime(p) and test(a, p) == a:
+        print('yes')
     else:
-        print('No')
+        print('no')
