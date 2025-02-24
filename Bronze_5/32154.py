ab1c6f9964a328d9ca127159432cb2fcd9e27827
KIM YOUNGROK <yr7256@gmail.com>
2025-02-25 03:16:14 +0900

    boj 32154

---
+++ Bronze_5/32154.py
@@ -0,0 +1,13 @@
+arr = [["11", "A B C D E F G H J L M"],
+       ["9", "A C E F G H I L M"],
+       ["9", "A C E F G H I L M"],
+       ["9", "A B C E F G H L M"],
+       ["8", "A C E F G H L M"],
+       ["8", "A C E F G H L M"],
+       ["8", "A C E F G H L M"],
+       ["8", "A C E F G H L M"],
+       ["8", "A C E F G H L M"],
+       ["8", "A B C F G H L M"]]
+N = int(input())
+print(arr[N-1][0])
+print(arr[N-1][1])
